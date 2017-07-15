f061400230d57aaa47f6c6f74a16160b153dd90e
Jasonzj <925645402@qq.com>
2017-07-16 00:56:33 +0800

    [concat]: add concat

---
+++ concat.js
@@ -0,0 +1,16 @@
+const concat = function(array) {
+    const result = array == null ? [] : Array.from(array)
+
+    Array.prototype.slice.call(arguments, 1).forEach(arg => {
+        if (arg instanceof Array) {
+            arg.forEach(a => {
+                result.push(a)
+            })
+            return 
+        } 
+        result.push(arg)
+    })
+
+    return result
+}
+
