132ce42d0ed9a2d105bb6d91f52cb967e896bc4d
Jasonzj <925645402@qq.com>
2017-09-28 17:21:55 +0800

    update intersection.js

--- intersection.js
+++ intersection.js
@@ -1,6 +1,7 @@
-const intersection = (...arrs) => 
+const intersection = (...arrs) => (
   arrs.reduce((pre, cur) =>
       arrs[0].filter((item) => 
           cur.includes(item) && pre.includes(item)
       )
   )
+)
