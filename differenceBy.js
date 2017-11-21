0a60d5d78637f6a0551ff06ec670cd69f39ad5e6
Jasonzj <925645402@qq.com>
2017-11-21 13:12:05 +0800

    [differenceBy]: update

--- differenceBy.js
+++ differenceBy.js
@@ -1,13 +1,20 @@
 const differenceBy = (arr, ...values) => {
   const result = []
   const iteratee = values.pop()
+  const isStr = typeof iteratee === 'string'
 
   values = []
     .concat(...values)
-    .map(val => iteratee(val))
-  
+    .map(val => {
+      if (isStr) return val[iteratee]
+      return iteratee(val)
+    })
+
   arr.forEach((item, i) => {
-    const _item = iteratee.call(null, item, i, arr)
+    if (!isStr) {
+      const _item = iteratee.call(null, item, i, arr)
+    }
+    const _item = item[iteratee]
     const diff = values.includes(_item)
 
     if (!diff) result.push(item)
